21e3e8ade8035b3400a807dc3cff6cb4fe3727ba
PatrickC05 <53089989+PatrickC05@users.noreply.github.com>
2022-05-23 20:05:44 -0400

    basic grid from tutorial shown

---
+++ plot.js
@@ -0,0 +1,43 @@
+var data = [
+	{
+		type: "scattergeo",
+		lat: [40.7127, 51.5072],
+		lon: [-74.0059, 0.1275],
+		mode: "lines",
+		line: {
+			width: 2,
+			color: "blue",
+		},
+	},
+];
+
+var layout = {
+	title: "London to NYC Great Circle",
+	showlegend: false,
+	geo: {
+		projection: {
+			type: "orthographic",
+			rotation: {
+				lon: -74,
+				lat: 40.7,
+			},
+		},
+		showocean: true,
+        oceancolor: 'rgb(0, 255, 255)',
+        showland: true,
+        landcolor: 'rgb(230, 145, 56)',
+        showlakes: true,
+        lakecolor: 'rgb(0, 255, 255)',
+        showcountries: true,
+        lonaxis: {
+            showgrid: true,
+            gridcolor: 'rgb(102, 102, 102)'
+        },
+        lataxis: {
+            showgrid: true,
+            gridcolor: 'rgb(102, 102, 102)'
+        }
+	},
+};
+
+Plotly.newPlot("myDiv", data, layout);
