da4e981631fb5cc8e20a9fe42cf14a6385b26ed8
PatrickC05 <53089989+PatrickC05@users.noreply.github.com>
2022-05-26 16:45:37 -0400

    works with random points

--- plot.js
+++ plot.js
@@ -1,33 +1,68 @@
-var data = [
-	{
+const initial_point = [40.7127, -74.0059];
+const OPACITY = 0.5;
+const START = 10;
+const WIDTH = 0.5;
+let current_points = [];
+var data = [];
+
+function gen_point(point) {
+	let new_lat = Math.random()*360-180;
+	let new_lon = Math.random()*360-180;
+	return [new_lat, new_lon];
+}
+
+for (let i = 0; i < START; i++) {
+	let new_point = gen_point(initial_point);
+	console.log(new_point);
+	data.push({
 		type: "scattergeo",
-		lat: [40.7127, 51.5072],
-		lon: [-74.0059, 0.1275],
+		lat: [initial_point[0], new_point[0]],
+		lon: [initial_point[1], new_point[1]],
 		mode: "lines",
 		line: {
-			width: 2,
-			color: "blue",
+			width: WIDTH,
+			color: "red",
 		},
-	},
-];
+		opacity: OPACITY,
+	});
+	current_points.push(new_point);
+}
+
+for (point of current_points) {
+	
+	for (let i = 0; i < START; i++) {
+		let new_point = gen_point(point);
+		data.push({
+			type: "scattergeo",
+			lat: [point[0], new_point[0]],
+			lon: [point[1], new_point[1]],
+			mode: "lines",
+			line: {
+				width: WIDTH,
+				color: "blue",
+			},
+			opacity: OPACITY,
+		});
+	}
+}
 
 var layout = {
     autosize: true,
 	showlegend: false,
 	geo: {
 		projection: {
-			type: "orthographic",
-			rotation: {
-				lon: -74,
-				lat: 40.7,
-			},
+			type: "equirectangular",
+			// rotation: {
+			// 	lon: -74,
+			// 	lat: 40.7,
+			// },
 		},
 		showocean: true,
-        oceancolor: 'rgb(127, 205, 255)',
+        oceancolor: 'rgb(100, 180, 230)',
         showland: true,
-        landcolor: 'rgb(0, 192, 0)',
+        landcolor: 'rgb(0, 210, 0)',
         showlakes: true,
-        lakecolor: 'rgb(127, 205, 255)',
+        lakecolor: 'rgb(100, 180, 230)',
         showcountries: true,
         lonaxis: {
             showgrid: true,
@@ -43,10 +78,52 @@ var layout = {
 	paper_bgcolor: 'rgb(20,22,22)',
 	plot_bgcolor: 'rgb(20,22,22)',
 };
-
+console.log(data)
 Plotly.newPlot("globe", data, layout);
 
 document.querySelector("#slider").addEventListener("input", function(e) {
 	var value = e.target.value;
 	document.querySelector("#num").innerHTML = value;
+});
+
+document.querySelector("#slider").addEventListener("change", function(e) {
+	var mydata = [];
+	var mycurrent_points = [];
+	var value = e.target.value;
+	console.log(value);
+	for (let i = 0; i < value; i++) {
+		let new_point = gen_point(initial_point);
+		mydata.push({
+			type: "scattergeo",
+			lat: [initial_point[0], new_point[0]],
+			lon: [initial_point[1], new_point[1]],
+			mode: "lines",
+			line: {
+				width: WIDTH,
+				color: "red",
+			},
+			opacity: OPACITY,
+		});
+		mycurrent_points.push(new_point);
+	}
+
+	for (point of mycurrent_points) {
+		
+		for (let i = 0; i < value; i++) {
+			let new_point = gen_point(point);
+			mydata.push({
+				type: "scattergeo",
+				lat: [point[0], new_point[0]],
+				lon: [point[1], new_point[1]],
+				mode: "lines",
+				line: {
+					width: WIDTH,
+					color: "blue",
+				},
+				opacity: OPACITY
+			});
+		}
+	}
+	Plotly.newPlot("globe", mydata, layout);
+	console.log(mydata)
 });
